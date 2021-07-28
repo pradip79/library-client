48e6d501f602cacc89efe5bbca4699345b194eca
Pradip patra <pradip.patra@gmail.com>
2021-07-28 09:03:51 +0530

    Back to initial state

--- src/test/java/com/testannotation/searchbooks/SearchBooksApplicationTests.java
+++ src/test/java/com/testannotation/searchbooks/SearchBooksApplicationTests.java
@@ -1,2 +1,12 @@
-package com.testannotation.searchbooks;public class SearchBooksApplicationTests {
+package com.testannotation.searchbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SearchBooksApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
 }
